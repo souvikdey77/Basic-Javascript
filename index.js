4f6070f97697a53ddace58abf0b48e159df65a4e
souvikdey77 <s09dey@gmail.com>
2020-11-13 02:15:46 +0530

    Javascript || Committed variable concepts

---
+++ index.js
@@ -0,0 +1,10 @@
+ // This is for comment
+ console.log('Hello World');
+ 
+//------Variable---------
+
+ //variable can't start with a number
+ //can't contain a space or hyphen
+ //case sensitive
+ let name = 'Aravind';
+ console.log(name);
